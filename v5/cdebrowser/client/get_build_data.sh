e2be00d221199a1aa49ece2a63f0d5d07b592d9f
lernermh <martin.lerner@nih.gov>
2015-07-28 15:02:28 -0400

    Cleaning up diagnostic output in get_build_data.sh

--- v5/cdebrowser/client/get_build_data.sh
+++ v5/cdebrowser/client/get_build_data.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# The Git build time was originally passed to maven with Git command in back ticks -Dscm_date=`git show --pretty=format:%ci master |sed -n 1p`
+# The Git commit date/time was originally passed to maven with Git command in back ticks -Dscm_date=`git show --pretty=format:%ci master |sed -n 1p` on the maven command line
 # AntHillPro did not execute the command within the back ticks.  The environment variable created by this command line argument was used
 # to populate a JSON file which provides the data for "Version Popup"
 
@@ -11,21 +11,14 @@ which git > /dev/null
 if [  $? -ne 0 ]
 then
    echo Git is not on the PATH
-   xport scm_date2=`/usr/local/bin/git show --pretty=format:%ci | sed -n 1p| sed 's/ \-/ \\\-/g'`
-
+   export scm_date2=`/usr/local/bin/git show --pretty=format:%ci | sed -n 1p| sed 's/ \-/ \\\-/g'`
 else
     export scm_date2=`git show --pretty=format:%ci | sed -n 1p| sed 's/ \-/ \\\-/g'`
 fi
 
-echo Update build time:  ${scm_date2}
-
 # Not using sed with -i because it does not work on Mac
 #sed -i "s/^scm-date.*/scm-date=${scm_date2}/g" src/main/resources/cdeBrowser.server.properties
 
-echo Copy src/main/webapp/version.json to  /tmp/version.json
 sed  "s/\"scm_date\":.*/\"scm_date\":\"${scm_date2}\"/g" src/main/webapp/version.json > /tmp/version.json
-ls -l /tmp/
-echo copy /tmp/version.json to src/main/webapp/version.json
 cp /tmp/version.json src/main/webapp/version.json
-cat src/main/webapp/version.json
 
