01e64d88d7331ae2c640419df7520e4ec6b1d94a
lernermh <martin.lerner@nih.gov>
2016-01-29 04:46:23 -0500

    Cleaned up sed for adding git date.

--- v5/cdebrowser/client/get_build_data.sh
+++ v5/cdebrowser/client/get_build_data.sh
@@ -5,6 +5,7 @@
 # to populate a JSON file which provides the data for "Version Popup"
 
 # This script is called by maven (configured in v5/cdebrowser/client/pom.xml)
+# Before Maven call this script, it copies version.template.json to version.json
 
 #if git is not on the PATH, then we are probably running within AntHillPro and it will be in /usr/loclal/bin
 which git > /dev/null
@@ -15,10 +16,8 @@ then
 else
     export scm_date2=`git show --pretty=format:%ci | sed -n 1p| sed 's/ \-/ \\\-/g'`
 fi
-echo Commit Date/Time: $scm_date2
-# Not using sed with -i because it does not work on Mac
-#sed -i "s/^scm-date.*/scm-date=${scm_date2}/g" src/main/resources/cdeBrowser.server.properties
 
-sed  "s/\"scm_date\":.*/\"scm_date\":\"${scm_date2}\"/g" src/main/webapp/version.json > /tmp/version.json
-cp /tmp/version.json src/main/webapp/version.json
+echo Commit Date/Time: $scm_date2
+
+sed -i  .json "s/\"scm_date\":.*/\"scm_date\":\"${scm_date2}\"/g" src/main/webapp/version.json 
 
