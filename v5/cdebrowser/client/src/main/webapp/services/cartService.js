815ccab818486a0be4e5ede9367aac4435a155fb
Shaun Einolf <seinolf@gmail.com>
2016-03-29 19:08:10 -0400

    changes to service

--- v5/cdebrowser/client/src/main/webapp/services/cartService.js
+++ v5/cdebrowser/client/src/main/webapp/services/cartService.js
@@ -2,10 +2,12 @@ angular.module("cdeBrowserApp").service('cartService', function() {
 	// service to create and operate a cde cart //
 	this.cartData = [];
 
-	this.deleteCDE = function(id) {
+	// delete cde's from cart //
+	this.deleteCDE = function(ids) {
 		this.cartData.pop();
 	};
 
+	// add cde's to cart. checkedItems is just an array of ids hence the need for searchResults //
 	this.addCDE = function(checkedItems, searchResults) {
 		for (var item in searchResults) {
 			if (checkedItems.indexOf(searchResults[item]['deIdseq'])>=0) {
@@ -16,10 +18,12 @@ angular.module("cdeBrowserApp").service('cartService', function() {
 		};
 	};	
 
+	// save the cart //
 	this.saveCart = function() {
 		console.log(this.cartData);
 	};
 
+	// retrieve the cart. Will call rest service //
 	this.retrieveCart = function() {
 		return this.cartData;
 	};		
