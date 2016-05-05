62d3b0dd523e302c19cf2e023c40ab0c544301d9
Subhash Vallala <sgvallala@gmail.com>
2016-05-05 10:29:49 -0400

    added Search Preferences tab

--- v5/cdebrowser/client/src/main/webapp/cdeBrowserClient.js
+++ v5/cdebrowser/client/src/main/webapp/cdeBrowserClient.js
@@ -104,7 +104,7 @@ angular.module("cdeBrowserApp").controller("cdeBrowserController", function ($wi
     ];
 
     $scope.activeSearchTab = 0;
-    $scope.searchTabs = [{title:"Data Element Search",disabled:false},{title:"Public ID Search",disabled:false}];
+    $scope.searchTabs = [{title:"Data Element Search",disabled:false},{title:"Public ID Search",disabled:false},{title:"Search Preferences",disabled:false}];
     $scope.tabs = [
         {
             title: 'Search Results',
