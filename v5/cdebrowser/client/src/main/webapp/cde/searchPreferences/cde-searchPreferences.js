ac0c80609182d55adde82fb92d89b89e6d8664e4
Subhash Vallala <sgvallala@gmail.com>
2016-05-26 19:18:49 -0400

    fixed POST rest call interaction with reset button after refresh and added view refreshing on click of button

--- v5/cdebrowser/client/src/main/webapp/cde/searchPreferences/cde-searchPreferences.js
+++ v5/cdebrowser/client/src/main/webapp/cde/searchPreferences/cde-searchPreferences.js
@@ -37,10 +37,8 @@ angular.module("cdeSearchPreferences").controller("SearchPreferencesController",
         //dataTransferService.setData("registrationStatusIncluded", $scope.registrationStatusIncluded);
 
 
+
         
-
-
-
 
 
 
@@ -52,19 +50,94 @@ angular.module("cdeSearchPreferences").controller("SearchPreferencesController",
 
         $scope.searchPreferencesCheckboxModel.excludeTest = $scope.defaultCheckboxModel.excludeTest;
         $scope.searchPreferencesCheckboxModel.excludeTraining = $scope.defaultCheckboxModel.excludeTraining;
-        // $scope.searchPreferencesCheckboxModel = $scope.defaultCheckboxModel;
-        $scope.models = $scope.defaultModels;
 
-        var resetRequestPayload = { };
-        resetRequestPayload.excludeTest = $scope.defaultCheckboxModel.excludeTest;
-        resetRequestPayload.excludeTraining = $scope.defaultCheckboxModel.excludeTraining;
-        resetRequestPayload.workflowStatusExcluded = $scope.defaultWorkflowStatusExcluded;
-        resetRequestPayload.registrationStatusExcluded = $scope.defaultRegistrationStatusExcluded;
-        $http.post('/cdebrowserServer/rest/searchPreferences',resetRequestPayload).then(function(response){
+          $scope.models = [
+        {
+            label: "workflowStatusIncluded",
+            allowedTypes: ['workflowStatus'],
+            items: [
+            ],dragging: false
+        },
+        {
+            label: "workflowStatusExcluded",
+            allowedTypes: ['workflowStatus'],
+            items: [
+            ],dragging: false
+        },
+        {
+            label: "registrationStatusIncluded",
+            allowedTypes: ['registrationStatus'],
+            items: [
+            ],dragging: false
+        },
+        {
+            label: "registrationStatusExcluded",
+            allowedTypes: ['registrationStatus'],
+            items: [
+            ],dragging: false
+        }];
+
+          $http.get('/cdebrowserServer/rest/searchPreferences/default').then(function(response) {
+              $scope.workflowStatusExcluded = response.data.workflowStatusExcluded;
+              $scope.registrationStatusExcluded = response.data.registrationStatusExcluded;
+              $scope.searchPreferencesCheckboxModel.excludeTest = response.data.excludeTest;
+              $scope.searchPreferencesCheckboxModel.excludeTraining = response.data.excludeTraining;
+              $scope.defaultWorkflowStatusExcluded = response.data.workflowStatusExcluded;
+              $scope.defaultRegistrationStatusExcluded = response.data.registrationStatusExcluded;
+              $scope.defaultCheckboxModel.excludeTest = response.data.excludeTest;
+              $scope.defaultCheckboxModel.excludeTraining = response.data.excludeTraining;     
+          }).then(function() { 
+                    $http.get('/cdebrowserServer/rest/lookupdata/workflowstatus').then(function(response) {
+                      $scope.workflowStatuses = response.data;
+                      $scope.workflowStatusIncluded = [];
+                      $scope.workflowStatusIncluded = $scope.workflowStatuses.filter(function(x){ return $scope.workflowStatusExcluded.indexOf(x)<0});
+                      angular.forEach($scope.models, function(list) {
+                          switch(list.label) {
+                            case "workflowStatusIncluded":
+                              for (var i = 0; i <= $scope.workflowStatusIncluded.length - 1; i++) {
+                                list.items.push({label: $scope.workflowStatusIncluded[i], type: "workflowStatus"});
+                              }
+                              break;
+                            case "workflowStatusExcluded":
+                              for (var i = 0; i <= $scope.workflowStatusExcluded.length - 1; i++) {
+                                list.items.push({label: $scope.workflowStatusExcluded[i], type: "workflowStatus"});
+                              }
+                              break;
+                          }  
+                    });
+                  });
+          }).then(function() {
+                    $http.get('/cdebrowserServer/rest/lookupdata/registrationstatus').then(function(response) {
+                      $scope.registrationStatuses = response.data;
+                      $scope.registrationStatusIncluded = [];
+                      $scope.registrationStatusIncluded = $scope.registrationStatuses.filter(function(x){ return $scope.registrationStatusExcluded.indexOf(x)<0});
+                      angular.forEach($scope.models, function(list) {
+                          switch(list.label) {
+                            case "registrationStatusIncluded":
+                              for (var i = 0; i <= $scope.registrationStatusIncluded.length - 2; i++) { // "length - 2" since the last object is empty
+                                list.items.push({label: $scope.registrationStatusIncluded[i], type: "registrationStatus"});
+                              }
+                              break;
+                            case "registrationStatusExcluded":
+                              for (var i = 0; i <= $scope.registrationStatusExcluded.length - 1; i++) {
+                                list.items.push({label: $scope.registrationStatusExcluded[i], type: "registrationStatus"});
+                              }
+                              break;
+                          }  
+                    });
+                  });
+          }).then(function(){
+
+          var resetRequestPayload = { };
+          resetRequestPayload.excludeTest = $scope.defaultCheckboxModel.excludeTest;
+          resetRequestPayload.excludeTraining = $scope.defaultCheckboxModel.excludeTraining;
+          resetRequestPayload.workflowStatusExcluded = $scope.defaultWorkflowStatusExcluded;
+          resetRequestPayload.registrationStatusExcluded = $scope.defaultRegistrationStatusExcluded;
+          $http.post('/cdebrowserServer/rest/searchPreferences',resetRequestPayload).then(function(response){
           console.log(response.statusText);
+          });
           // $scope.$emit('basicSearchEvent', query, field, type, publicIdName);
         });
-
      };
 
 
@@ -72,7 +145,7 @@ angular.module("cdeSearchPreferences").controller("SearchPreferencesController",
   // $scope.Statuses is for rest calls (workflow and registration) and $scope.Status is for variables (workflowIncluded,
   // workflowExcluded, registrationIncluded and registrationExcluded)
   
-    $http.get('/cdebrowserServer/rest/searchPreferences').then(function(response) { 
+    $http.get('/cdebrowserServer/rest/searchPreferences').then(function(response) { // /default
         $scope.workflowStatusExcluded = response.data.workflowStatusExcluded;
         $scope.registrationStatusExcluded = response.data.registrationStatusExcluded;
         $scope.searchPreferencesCheckboxModel.excludeTest = response.data.excludeTest;
@@ -185,10 +258,6 @@ angular.module("cdeSearchPreferences").controller("SearchPreferencesController",
         $scope.modelsAsJson = angular.toJson(model, true);
         $scope.modifiedModels = $scope.modelsAsJson;   
     }, true);
-
-    // $scope.defaultModels = angular.toJson($scope.models);
-    // $scope.defaultCheckboxModel = angular.copy($scope.searchPreferencesCheckboxModel);
-
 
 
 
