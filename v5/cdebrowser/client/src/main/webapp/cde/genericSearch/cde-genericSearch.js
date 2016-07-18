41368d5acdd77fc1a0e5605e201d0d1d2d5db9ac
lernermh <martin.lerner@nih.gov>
2016-07-18 09:50:59 -0400

    Add to search Object class and Context use

--- v5/cdebrowser/client/src/main/webapp/cde/genericSearch/cde-genericSearch.js
+++ v5/cdebrowser/client/src/main/webapp/cde/genericSearch/cde-genericSearch.js
@@ -1,23 +1,28 @@
 angular.module("cdeGenericSearch", []);
 
 angular.module("cdeGenericSearch").controller("GenericSearchController", function ($scope, filterService) {
-    
+
     $scope.fs = filterService;
-    
+
     $scope.$watch('isNode', function () {
 		$scope.basicSearchQuery = "";
     });
-    
-    
+
+
     $scope.$watch('selectedQueryType', function () {
     	$scope.fs.dataElementVariables;
-    });    
-    
+    });
+
     $scope.searchFieldOptions={};
     $scope.searchFieldOptions.options=[];
     $scope.options = ["ALL", "Long Name", "Short Name", "Preferred Question Text", "Alternate Question Text", "UML Class: UML Attr Alternate Name"];
 
     $scope.searchFieldOptions.options[0] = $scope.options[0];
 
+ /*
+    Can't do it this way, sometimes $scope.alternateNameTypes is not yet populated.
+    $scope.fs.dataElementVariables.searchAltNameType = $scope.alternateNameTypes[0];
+*/
+    $scope.fs.dataElementVariables.searchAltNameType = {"type": "ALL"};
 
 });
