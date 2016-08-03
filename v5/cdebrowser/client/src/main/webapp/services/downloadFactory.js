a5d557a9afaaa9400d542850ccb2da2af0ad102c
Subhash Vallala <sgvallala@gmail.com>
2016-08-02 23:26:47 -0400

    CDEBROWSER-509 changes

--- v5/cdebrowser/client/src/main/webapp/services/downloadFactory.js
+++ v5/cdebrowser/client/src/main/webapp/services/downloadFactory.js
@@ -38,7 +38,7 @@ angular.module("cdeBrowserApp").factory('downloadFactory', function($http) {
 	     	this.progressMessage = {"status":1,"message":"Exporting Data", "isErrorMessage":0};
 	     	var that = this;
 
-	            $http({method: 'POST', url: '/cdebrowserServer/rest/excelDownload',data: items}).
+	            $http({method: 'POST', url: '/cdebrowserServer/rest/downloadExcel?src=cdeCompare',data: items}).
 	            success(function(data, status, headers, config) {
 	           	that.progressMessage.status=0;
 	                 window.location.href = window.location.protocol + "//" + window.location.hostname + ":" + window.location.port + "/cdebrowserServer/rest/downloadExcel/" + data;
