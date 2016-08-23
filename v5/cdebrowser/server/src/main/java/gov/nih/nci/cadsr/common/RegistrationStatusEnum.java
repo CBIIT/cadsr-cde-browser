e8fdf8f9022e4590808c16f54b1d422d30e9cbda
vikramsan <vikram.san@gmail.com>
2016-08-23 09:29:30 -0400

    Adding 'Preferred Standard" to the list of Registration statuses

--- v5/cdebrowser/server/src/main/java/gov/nih/nci/cadsr/common/RegistrationStatusEnum.java
+++ v5/cdebrowser/server/src/main/java/gov/nih/nci/cadsr/common/RegistrationStatusEnum.java
@@ -6,6 +6,7 @@ import java.util.List;
 public enum RegistrationStatusEnum
 {
 	STANDARD("Standard"),
+	PREFSTD("Preferred Standard"),	
 	CANDIDATE("Candidate"),
 	PROPOSED("Proposed"),
 	QUALIFIED("Qualified"),
