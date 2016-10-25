936a8ac6e10b30a283b79c53960cca474e919f84
asafieva <natalia.asafieva@nih.gov>
2016-10-25 14:56:56 -0400

    CDEBROWSER-553 DE Link using search route to retrieve DE details by a URL with parameters

--- v5/cdebrowser/client/src/main/webapp/cde/dataElement/cde-dataElement.js
+++ v5/cdebrowser/client/src/main/webapp/cde/dataElement/cde-dataElement.js
@@ -3,28 +3,13 @@
  */
 
 angular.module("cdeDataElement", []);
-//TODO this code tries to create a direct link to a DE details view by DE Public ID and Version. It is a test implementation, it does not really work
+//this code creates a direct link to a DE details view by DE Public ID and Version.
 angular.module("cdeDataElement").controller("DataElementCtrl", ["$scope", "$location", "$http", function ($scope, $location, $http) {
     $scope.getCdeData = function () {
-    	$scope.publicId = $location;
-    	var searchObject = $location.search();
-    	console.log("cde-dataElement.js debug log searchObject.publicId: " + searchObject.publicId + ", searchObject.version: " + searchObject.version );
-    	if (($location.search().hasOwnProperty('publicId')) && ($location.search().hasOwnProperty('version'))) {
-    		var dataElementServerLink = window.location.protocol + "//" + window.location.hostname + ":" + window.location.port 
-        		+ "/cdebrowserServer/rest/CDELink?publicId=" + searchObject.publicId+"&version=" + searchObject.version;
-    		console.log("cde-dataElement.js debug log dataElementServerLink: " + dataElementServerLink);
     		$scope.dataElementLink = window.location.protocol + "//" + window.location.hostname + ":" + window.location.port 
-			+ "/cdebrowserClient/cdeBrowser.html#/dataElement?publicId=" + searchObject.publicId +"&version=" 
-				+ searchObject.version;
-    		console.log("cde-dataElement.js debug log $scope.dataElementLink: " + $scope.dataElementLink);
-    		$scope.getCdeDetailRestCall(dataElementServerLink);
-    	}
-    	else {
-    		$scope.dataElementLink = window.location.protocol + "//" + window.location.hostname + ":" + window.location.port 
-    			+ "/cdebrowserClient/cdeBrowser.html#/dataElement?publicId=" + $scope.cdeDetails.dataElement.dataElementDetails.publicId +"&version=" 
+    			+ "/cdebrowserClient/cdeBrowser.html#/search?publicId=" + $scope.cdeDetails.dataElement.dataElementDetails.publicId +"&version=" 
     				+ $scope.cdeDetails.dataElement.dataElementDetails.formattedVersion;
-    		console.log("cde-dataElement.js dataElementLink: " + $scope.dataElementLink);
-    	}
+    		console.log("cde-dataElement.js dataElementLink: " + $scope.dataElementLink);    	
     };
 
     $scope.getCdeData();
