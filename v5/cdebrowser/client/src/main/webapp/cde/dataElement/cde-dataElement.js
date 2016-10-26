db25546c7e21a3fcc074e0bc1722b1ec9a26a552
asafieva <natalia.asafieva@nih.gov>
2016-10-26 14:36:47 -0400

    CDEBROWSER-553 DE Link error handling related fixes

--- v5/cdebrowser/client/src/main/webapp/cde/dataElement/cde-dataElement.js
+++ v5/cdebrowser/client/src/main/webapp/cde/dataElement/cde-dataElement.js
@@ -6,13 +6,12 @@ angular.module("cdeDataElement", []);
 //this code creates a direct link to a DE details view by DE Public ID and Version.
 angular.module("cdeDataElement").controller("DataElementCtrl", ["$scope", "$location", "$http", function ($scope, $location, $http) {
     $scope.getCdeData = function () {
-    	if ($scope.cdeDetails != null && $scope.cdeDetails.dataElementDetails != null) {
-    		$scope.dataElementLink = window.location.protocol + "//" + window.location.hostname + ":" + window.location.port 
+    	$scope.dataElementLink = window.location.protocol + "//" + window.location.hostname + ":" + window.location.port 
     			+ "/cdebrowserClient/cdeBrowser.html#/search?publicId=" + $scope.cdeDetails.dataElement.dataElementDetails.publicId +"&version=" 
     				+ $scope.cdeDetails.dataElement.dataElementDetails.formattedVersion;
-    	}
+    	console.log("cde-dataElement.js scope.dataElementLink: " + $scope.dataElementLink);    	
     };
-
-    $scope.getCdeData();
-
+    if ($scope.cdeDetails.dataElement != null) {
+    	$scope.getCdeData();
+	}
 }]);
