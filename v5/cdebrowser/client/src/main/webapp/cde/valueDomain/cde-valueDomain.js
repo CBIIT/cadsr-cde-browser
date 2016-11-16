138bbfba14c2106a6b29300c5b1e1ab8c1bd89b0
Subhash Vallala <sgvallala@gmail.com>
2016-11-16 12:28:13 -0500

    CDEBROWSER-712 changes

--- v5/cdebrowser/client/src/main/webapp/cde/valueDomain/cde-valueDomain.js
+++ v5/cdebrowser/client/src/main/webapp/cde/valueDomain/cde-valueDomain.js
@@ -4,12 +4,9 @@ angular.module("cdeValueDomain", []);
 angular.module("cdeValueDomain").controller("ValueDomainCtrl", ["$scope", "$anchorScroll", "$location", function ($scope, $anchorScroll, $location) {
 	
 	$scope.goToVM = function(i) {
-		$scope.changeView(4,$scope.tabs[4]);
-		console.log(i);
-		var change = $location.hash();
-        $location.hash(i);
+		$scope.changeView(4, $scope.tabs[4]);
         $anchorScroll();
-        $location.hash(change);
+        $location.hash(i);
 	};
-	
+
 }]);
