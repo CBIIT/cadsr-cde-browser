7c16e402a5a7597d7a30693d25499da29676d662
asafieva <natalia.asafieva@nih.gov>
2017-05-02 17:08:29 -0400

    CDEBROWSER-808 preliminary DAO changes for typeahead

--- v5/cdebrowser/server/src/main/java/gov/nih/nci/cadsr/dao/TypeaheadSearchDAOImpl.java
+++ v5/cdebrowser/server/src/main/java/gov/nih/nci/cadsr/dao/TypeaheadSearchDAOImpl.java
@@ -6,16 +6,24 @@ package gov.nih.nci.cadsr.dao;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Set;
 
 import javax.sql.DataSource;
 
 import org.apache.commons.lang3.StringUtils;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
 
+import gov.nih.nci.cadsr.common.util.StringUtilities;
 import gov.nih.nci.cadsr.dao.operation.AbstractDAOOperations;
+import gov.nih.nci.cadsr.service.model.search.SearchCriteria;
 /**
  * This class provides DB support for Typeahead search feature
  * CDEBROWSER-808
@@ -23,9 +31,9 @@ import gov.nih.nci.cadsr.dao.operation.AbstractDAOOperations;
  *
  */
 public class TypeaheadSearchDAOImpl extends AbstractDAOOperations implements TypeaheadSearchDAO {
-	//private static final Logger logger = LogManager.getLogger(TypeaheadDAOImpl.class.getName());
+	private static final Logger logger = LogManager.getLogger(TypeaheadSearchDAOImpl.class.getName());
+	
 	private static final int maxLongNamesToReturn = 20;
-
 	private JdbcTemplate jdbcTemplate;
 	//TODO we need to consider Search Preferences when we generate typeahead results
 	//see methods in SearchQueryBuilder as initSearchQueryBuilder, buildSearchTextWhere
@@ -36,7 +44,7 @@ public class TypeaheadSearchDAOImpl extends AbstractDAOOperations implements Typ
 			"where (substr(lower(long_name), 1, "
 			+ maxLongNamesToReturn
 			+ ") like ? ) order by th) where rownum < "
-			+ maxLongNamesToReturn;	
+			+ maxLongNamesToReturn;
 	public static final String sqlRetrieveTypeaheadLongName = "select th from (select distinct substr(lower(long_name), 1, 20) th from sbr.data_elements " + 
 			"where (instr(substr(lower(long_name), 1, "
 			+ maxLongNamesToReturn
@@ -52,6 +60,64 @@ public class TypeaheadSearchDAOImpl extends AbstractDAOOperations implements Typ
 			"where instr(lower(long_name), ?, 1) > 0 order by th) where rownum < "
 			+ maxLongNamesToReturn;
 	
+	//These are SQLs for final implementation
+	public static final String ASL_NAME_NOT_USED = " AND de.ASL_NAME != 'RETIRED DELETED'";
+	public static final String START_TYPEAHEAD_SELECT = "select th from (";
+	public static final String END_TYPEAHEAD_SELECT = ") where rownum < " + maxLongNamesToReturn;
+	private static final String UNION = "UNION ";
+	public static final String sqlRetrieveLongName = 
+			UNION
+			+ "(select distinct lower(de.long_name) th from sbr.data_elements de" + 
+			"WHERE "
+			+ "instr(UPPER(de.long_name), ?, 1) > 0 "
+			+ ASL_NAME_NOT_USED
+			+ "order by th) ";
+	
+	public static final String sqlRetrieveShortName = 
+			UNION
+			+ "(select distinct lower(de.short_name) th from sbr.data_elements de" + 
+			"WHERE "
+			+ "instr(UPPER(de.short_name), ?, 1) > 0 "
+			+ ASL_NAME_NOT_USED
+			+ "order by th) ";
+	
+	public static final String sqlRetrievePrefQuestionText = 
+			UNION
+			+ "(select distinct lower(rd1.doc_text) th from sbr.reference_documents_view rd1, sbr.data_elements de "
+			+ "WHERE "
+			+ "rd1.ac_idseq = de.de_idseq " //we are interested only in Ref docs which connect to CDEs
+			+ "AND instr(UPPER(rd1.doc_text), ?, 1) > 0 ) "
+			+ "AND rd1.dctl_name = 'Preferred Question Text' "
+			+ ASL_NAME_NOT_USED
+			+ "order by th) ";
+	
+	public static final String sqlRetrieveAltQuestionText = 
+			UNION
+			+ "(select distinct lower(rd2.doc_text) th from sbr.reference_documents_view rd2, sbr.data_elements de "
+			+ "WHERE "
+			+ "rd2.ac_idseq = de.de_idseq " //we are interested only in Ref docs which connect to CDEs
+			+ "AND instr(UPPER(rd2.doc_text), ?, 1) > 0 ) "
+			+ "AND rd2.dctl_name = 'Alternate Question Text' "
+			+ ASL_NAME_NOT_USED
+			+ "order by th) ";
+	
+	public static final String sqlRetrieveUMLStartsWith = 
+			UNION
+			+ "(SELECT dsn.name th FROM sbr.designations_view dsn, sbr.data_elements de "
+			+ "WHERE "
+			+ "dsn.ac_idseq = de.de_idseq "
+			+ "AND instr(UPPER(dsn.name), ?, 1) > 0 ) "
+			+ "AND dsn.detl_name = 'UML Class:UML Attr' "
+			+ "order by th) ";
+	private static final Map<String, String> filterInputSqls = new HashMap<>();
+	{
+		filterInputSqls.put("Long Name", sqlRetrieveLongName);
+		filterInputSqls.put("Short Name", sqlRetrieveShortName);
+		filterInputSqls.put("Preferred Question Text", sqlRetrievePrefQuestionText);
+		filterInputSqls.put("Alternate Question Text", sqlRetrieveAltQuestionText);
+		filterInputSqls.put("UML Class: UML Attr Alternate Name", sqlRetrieveUMLStartsWith);
+	}
+
 	@Autowired
 	TypeaheadSearchDAOImpl(DataSource dataSource) {
 		setDataSource(dataSource);
@@ -99,5 +165,71 @@ public class TypeaheadSearchDAOImpl extends AbstractDAOOperations implements Typ
 			return models1;
 		}
 		return new ArrayList<String>();	
+	}
+	public List<String> buildSearchTypeaheadName(String pattern, SearchCriteria searchCriteria) {
+		String fileterdInput = searchCriteria.getFilteredinput();
+		if ((StringUtils.isNotEmpty(pattern)) && (StringUtils.isNotEmpty(fileterdInput))) {
+			String[] searchDomain = fileterdInput.split( "\\s*,\\s*" );
+			if (searchDomain.length > 0) {
+				String sqlForTypeahead = buildTypeaheadDomainSql(searchDomain);
+				if (sqlForTypeahead != null) {
+					List<String> models1;
+					models1 = jdbcTemplate.query(sqlForTypeahead, new Object[]{pattern.toUpperCase()}, new StringPropertyMapper(String.class));
+					logger.debug("buildSearchTypeaheadName found matches: " + models1.size() + models1);
+					return models1;
+				}
+			}
+		}
+		return new ArrayList<String>();	
+	}
+	protected static String buildTypeaheadDomainSql(String[] searchDomain) {
+		List<String> domainSqlList = new ArrayList<>();
+		String res = null;
+		if (StringUtilities.containsKey(searchDomain, "ALL")) {
+			res = buildAlldDomainSql();
+		}
+		else {
+			for (String curr : searchDomain) {
+				if (curr != null) {
+					String sqlCurr = filterInputSqls.get(curr);
+					if (sqlCurr != null) {
+						domainSqlList.add(sqlCurr);
+					}
+					else {
+						logger.error("buildTypeaheadDomainSql unknown filterInput value: " + curr);
+					}
+				}
+			}
+			if (domainSqlList.size() > 0) {
+				StringBuilder sb = new StringBuilder();
+				StringBuilder sbUnion = new StringBuilder();
+				for (String curr : domainSqlList) {
+					sbUnion.append(curr);		
+				}
+				sbUnion.delete(0, UNION.length());
+				sb.append(START_TYPEAHEAD_SELECT);
+				sb.append(sbUnion);
+				sb.append(END_TYPEAHEAD_SELECT);
+				res = sb.toString();
+				logger.debug("buildTypeaheadDomainSql: " + res);
+			}
+		}
+		return res;
+	}
+	protected static String buildAlldDomainSql() {
+		StringBuilder sb = new StringBuilder();
+		StringBuilder sbUnion = new StringBuilder();
+		sbUnion.append(sqlRetrieveLongName);
+		sbUnion.append(sqlRetrieveShortName);
+		sbUnion.append(sqlRetrievePrefQuestionText);
+		sbUnion.append(sqlRetrieveAltQuestionText);
+		sbUnion.append(sqlRetrieveUMLStartsWith);
+		sbUnion.delete(0, UNION.length());
+		sb.append(START_TYPEAHEAD_SELECT);
+		sb.append(sbUnion);
+		sb.append(END_TYPEAHEAD_SELECT);
+		String resSql = sb.toString();
+		logger.debug("buildAlldDomainSql: " + resSql);
+		return resSql;
 	}
 }
