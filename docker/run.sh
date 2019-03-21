0db9d46efd6e1a2e951af9633bb33254fa1a250b
Shaun Einolf <einolfs@mail.nih.gov>
2019-03-21 08:26:53 -0400

    checking in some more docker stuff;

--- docker/run.sh
+++ docker/run.sh
@@ -1,3 +1,12 @@
 #~/bin/sh
+# if [ -d "/usr/src/cdebrowser/cadsr-cde-browser" ]; then
+#         echo "Skipping cadsr-cde-browser install"
+#         cd /usr/src/cdebrowser/cadsr-cde-browser
+#         git pull
+#         echo "pulling new code"
+# else
+#   git clone https://github.com/CBIIT/cadsr-cde-browser 
+#   echo "Cloning the repository"
+# fi
 
 exec /opt/wildfly/bin/standalone.sh -b 0.0.0.0 -bmanagement 0.0.0.0
