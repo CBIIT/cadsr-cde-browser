5f7f95cdc552fd11ca1ede8ad13135d39ce696f2
vikramsan <vikram.san@gmail.com>
2022-06-23 15:19:07 -0400

    Changes to CDE cart creation

--- v5/cdebrowser/server/src/main/java/gov/nih/nci/cadsr/cdecart/CdeCartUtil.java
+++ v5/cdebrowser/server/src/main/java/gov/nih/nci/cadsr/cdecart/CdeCartUtil.java
@@ -120,17 +120,17 @@ public class CdeCartUtil implements CdeCartUtilInterface {
 				}
 			}
 			ObjectCartClient ocClient = null;
-				try {
-					if (ocURL != null) {
+				//try {
+					//if (ocURL != null) {
 						log.debug("Creating OC Client - Object Cart URL:" + ocURL);				
-						ocClient = new ObjectCartClient(ocURL); 
-					} else {
-						ocClient = new ObjectCartClient();
-					}
-				} catch (Exception ex) {
+					//	ocClient = new ObjectCartClient(ocURL); 
+					//} else {
+						//ocClient = new ObjectCartClient();
+					//}
+				/*} catch (Exception ex) {
 					ex.printStackTrace();
 					log.debug("Unable to create Object Cart:" + ocURL);
-				}
+				}*/
 			uid = principalName;
 
 			//we shall be after login here, and uid is never null
