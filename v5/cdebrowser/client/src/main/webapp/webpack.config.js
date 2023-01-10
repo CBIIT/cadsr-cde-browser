65d470d0a192f6b02cd19a64b95ed60d7312b0bc
Shaun Einolf <seinolf@gmail.com>
2023-01-10 11:40:35 -0500

    adding new js libraries

--- v5/cdebrowser/client/src/main/webapp/webpack.config.js
+++ v5/cdebrowser/client/src/main/webapp/webpack.config.js
@@ -1,18 +1,16 @@
 module.exports = {
   entry: {
     "angular.min":'./node_modules/angular/angular.min.js',
-    "angular-animate.min":'./node_modules/angular-animate/angular-animate.min.js'
+    "angular-animate.min":'./node_modules/angular-animate/angular-animate.min.js',
     "angular-aria":"./node_modules/angular-aria/angular-aria.min.js",
-    "angular-cookies:"./node_modules/angular-cookies/angular-cookies.min.js",
-    "angular-i18n":"./node_modules/angular-i18n/angular-i18n.min.js",
+    "angular-cookies":"./node_modules/angular-cookies/angular-cookies.min.js",
     "angular-message-format":"./node_modules/angular-message-format/angular-message-format.min.js",
     "angular-messages":"./node_modules/angular-messages/angular-messages.min.js",
     "angular-parse-ext":"./node_modules/angular-parse-ext/angular-parse-ext.min.js",
     "angular-resource":"./node_modules/angular-resource/angular-resource.min.js",
     "angular-route":"./node_modules/angular-route/angular-route.min.js",
     "angular-sanitize":"./node_modules/angular-sanitize/angular-sanitize.min.js",
-    "angular-touch":"./node_modules/angular-touch/angular-touch.min.js",
-    "angular-mocks":"./node_modules/angular-mocks/angular-mocks.min.js"
+    "angular-touch":"./node_modules/angular-touch/angular-touch.min.js"
   },
   mode: 'production',
   output: {
